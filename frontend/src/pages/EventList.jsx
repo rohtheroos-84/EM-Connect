2765d6016ec1d90f70412825e27db5477b6d3541
rohit <rohit84.official@gmail.com>
2026-02-23 21:33:28 +0530

    removed search icon

--- frontend/src/pages/EventList.jsx
+++ frontend/src/pages/EventList.jsx
@@ -92,7 +92,6 @@ export default function EventList() {
 
           {/* Search */}
           <div className="relative">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-[#BCBCBC] pointer-events-none" />
             <input
               type="text"
               value={search}
